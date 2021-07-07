85343ddbf4f823f4b3fdb0f16c6a219f188969a9
ASPrakash <akhourisoumya@gmail.com>
2021-07-08 03:57:41 +0530

    Background Animations

--- myscript.js
+++ myscript.js
@@ -9,21 +9,26 @@ const back = document.getElementById("bod");
 const tab = document.getElementById("tab");
 const secSwitch = document.getElementById("secSwitch");
 const ggle = document.getElementById("gle");
+const circle = document.getElementById("circle");
 
 signUpBtn.onclick = (() => {
     loginForm.style.marginLeft = "-50%";
     loginTitle.style.marginLeft = "-50%";
     back.style.background = '-webkit-linear-gradient(right,#f72585,#7209b7)';
     tab.style.background = '-webkit-linear-gradient(left,#f72585,#7209b7)';
-    back.style.transition = 'background-color 3s'
+    circle.style.background = '-webkit-linear-gradient(right,#4361ee,#4cc9f0)';
+    circle.style.marginLeft = "125%";
+    circle.style.marginTop = "75%";
 });
 
 loginBtn.onclick = (() => {
     loginForm.style.marginLeft = "0%";
     loginTitle.style.marginLeft = "0%";
-    back.style.background = '-webkit-linear-gradient(left,#4361ee,#4cc9f0)';
+    // back.style.background = '-webkit-linear-gradient(left,#4361ee,#4cc9f0)';
     tab.style.background = '-webkit-linear-gradient(right,#4361ee,#4cc9f0)';
-    back.style.transition = 'background-color 3s'
+    // circle.style.background = '-webkit-linear-gradient(left,#f72585,#7209b7)';
+    circle.style.marginLeft = "0";
+    circle.style.marginTop = "0";
 });
 
 function verifyPass() {
